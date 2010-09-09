cb4fc07a98598afc1bf6ebc12329496717c83f31
ygfperson <ygfperson@04fcf5f6-408b-428b-944a-e74021d52a6c>
2010-09-09 00:45:28 +0000

    updating route info
    git-svn-id: https://bostonbusmap.svn.sourceforge.net/svnroot/bostonbusmap@238 04fcf5f6-408b-428b-944a-e74021d52a6c

--- bostonBusMap/src/boston/Bus/Map/data/Locations.java
+++ bostonBusMap/src/boston/Bus/Map/data/Locations.java
@@ -147,7 +147,7 @@ public final class Locations
 			*/
 			task.publish("Decompressing route data. This may take a minute...");
 			
-			final int contentLength = 407615;
+			final int contentLength = 453754;
 			
 			InputStream in = new StreamCounter(context.getResources().openRawResource(boston.Bus.Map.R.raw.routeconfig),
 					task, contentLength, null, "Decompressing route data, may take a minute: "); 
