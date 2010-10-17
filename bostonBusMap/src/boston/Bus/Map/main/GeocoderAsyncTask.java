d494060e17b64c5741312c5c591d0854584c6387
George Schneeloch <bostonbusmap@gmail.com>
2010-10-17 00:31:17 -0400

    adding geocoding

---
+++ bostonBusMap/src/boston/Bus/Map/main/GeocoderAsyncTask.java
@@ -0,0 +1,78 @@
+package boston.Bus.Map.main;
+
+import java.io.IOException;
+import java.util.List;
+
+import boston.Bus.Map.util.Constants;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+public class GeocoderAsyncTask extends AsyncTask<Object, String, GeoPoint> {
+	private final Context context;
+	private final MapView mapView;
+	private final String query;
+	
+	public GeocoderAsyncTask(Context context, MapView mapView, String query) {
+		this.context = context;
+		this.mapView = mapView;
+		this.query = query;
+	}
+
+	@Override
+	protected void onProgressUpdate(String... values) {
+		Toast.makeText(context, values[0], Toast.LENGTH_LONG).show();
+	}
+	
+	@Override
+	protected GeoPoint doInBackground(Object... params) {
+		Geocoder geocoder = new Geocoder(context);
+		try
+		{
+			List<Address> addresses = geocoder.getFromLocationName(query, 1);
+			if (addresses.size() < 1)
+			{
+				publishProgress("No results found for \"" + query + "\"");
+			}
+			else
+			{
+				Address address = addresses.get(0);
+				double lon = address.getLongitude();
+				double lat = address.getLatitude();
+				
+				String name = address.getAddressLine(0);
+				if (name == null)
+				{
+					name = "null";
+				}
+				publishProgress("Found a result: " + name);
+				
+				GeoPoint point = new GeoPoint((int)lat * Constants.E6, (int)lon * Constants.E6);
+				return point;
+			}
+		}
+		catch (IOException e)
+		{
+			//this is not important enough to risk crashing the phone
+			Log.e("BostonBusMap", e.getMessage());
+			
+			publishProgress("An unknown error occurred while searching for \"" + query + "\"");
+		}
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(GeoPoint point) {
+		if (point != null)
+		{
+			mapView.getController().animateTo(point);
+		}
+	}
+}
--- bostonBusMap/src/boston/Bus/Map/main/Main.java
+++ bostonBusMap/src/boston/Bus/Map/main/Main.java
@@ -42,6 +42,7 @@ import boston.Bus.Map.ui.LocationOverlay;
 import boston.Bus.Map.ui.ModeAdapter;
 import boston.Bus.Map.ui.RouteOverlay;
 import boston.Bus.Map.ui.ViewingMode;
+import boston.Bus.Map.util.Constants;
 
 
 import com.google.android.maps.GeoPoint;
@@ -62,7 +63,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
@@ -745,64 +748,111 @@ public class Main extends MapActivity
 				return;
 			}
 			
-			String lowercaseQuery = query.toLowerCase();
-			
-			//remove these words from the search
-			String[] wordsToRemove = new String[] {"route", "subway", "bus", "line"};
-
-			for (String wordToRemove : wordsToRemove)
+			int routeIndex = searchRoutes(query);
+			if (routeIndex == IS_GREEN_LINE)
 			{
-				if (lowercaseQuery.contains(wordToRemove))
-				{
-					query = query.replaceAll(wordToRemove, "");
-					lowercaseQuery = query.toLowerCase();
-				}
+				//we know what this is, don't try to search for it
 			}
-			
-			if (query.contains(" "))
+			else if (routeIndex == IS_NUMBER)
 			{
-				query = query.replaceAll(" ", "");
-				lowercaseQuery = query.toLowerCase();
+				//user probably mistyped a route number
 			}
-			
-			//indexingQuery is query which may be slightly altered to match one of the route keys
-			String indexingQuery = lowercaseQuery;
-			if (indexingQuery.length() >= 2)
+			else if (routeIndex == IS_NOTHING)
 			{
-				//this is kind of a hack. We need subway lines to start with a capital letter to search for them properly
-				indexingQuery = indexingQuery.substring(0, 1).toUpperCase() + query.substring(1);
-			}
-			
-			
-			int position = Arrays.asList(dropdownRoutes).indexOf(indexingQuery);
-			if (position != -1)
-			{
-				setNewRoute(position);
-				modeSpinner.setSelection(position);
+				//ok, try geocoding
+				
+				GeocoderAsyncTask geocoderAsyncTask = new GeocoderAsyncTask(this, mapView, query);
+				geocoderAsyncTask.execute();
 			}
 			else
 			{
-				//try the titles
-				for (int i = 0; i < dropdownRoutes.length; i++)
+				//it's a route!
+				setNewRoute(routeIndex);
+				modeSpinner.setSelection(routeIndex);
+			}
+		}
+	}
+
+	private static final int IS_GREEN_LINE = -1;
+	private static final int IS_NOTHING = -2;
+	private static final int IS_NUMBER = -3;
+	
+	/**
+	 * Try a search on the list of routes. If it matches, do that. Else, it's a geocode
+	 * 
+	 * @param query
+	 * @return
+	 */
+	private int searchRoutes(String query) {
+		String lowercaseQuery = query.toLowerCase();
+		
+		//remove these words from the search
+		String[] wordsToRemove = new String[] {"route", "subway", "bus", "line"};
+
+		for (String wordToRemove : wordsToRemove)
+		{
+			if (lowercaseQuery.contains(wordToRemove))
+			{
+				query = query.replaceAll(wordToRemove, "");
+				lowercaseQuery = query.toLowerCase();
+			}
+		}
+		
+		if (query.contains(" "))
+		{
+			query = query.replaceAll(" ", "");
+			lowercaseQuery = query.toLowerCase();
+		}
+		
+		//indexingQuery is query which may be slightly altered to match one of the route keys
+		String indexingQuery = lowercaseQuery;
+		if (indexingQuery.length() >= 2)
+		{
+			//this is kind of a hack. We need subway lines to start with a capital letter to search for them properly
+			indexingQuery = indexingQuery.substring(0, 1).toUpperCase() + query.substring(1);
+		}
+		
+		
+		int position = Arrays.asList(dropdownRoutes).indexOf(indexingQuery);
+		if (position != -1)
+		{
+			return position;
+		}
+		else
+		{
+			//try the titles
+			for (int i = 0; i < dropdownRoutes.length; i++)
+			{
+				String title = dropdownRouteKeysToTitles.get(dropdownRoutes[i]);
+				if (title != null && title.toLowerCase().equals(lowercaseQuery))
 				{
-					String title = dropdownRouteKeysToTitles.get(dropdownRoutes[i]);
-					if (title != null && title.toLowerCase().equals(lowercaseQuery))
-					{
-						setNewRoute(i);
-						modeSpinner.setSelection(i);
-						return;
-					}
+					return i;
+				}
+			}
+			
+			//else, we don't know what it is
+			if (lowercaseQuery.equals("green"))
+			{
+				Toast.makeText(this, "Sorry, green line information isn't available yet", Toast.LENGTH_LONG).show();
+				return IS_GREEN_LINE;
+			}
+			else
+			{
+				try
+				{
+					int x = Integer.parseInt(lowercaseQuery);
+					Toast.makeText(this, "Route number '" + x + "' doesn't exist. Did you mistype it?", Toast.LENGTH_LONG).show();
+					return IS_NUMBER;
+				}
+				catch (NumberFormatException e)
+				{
+					//no need to log this, it's mostly expected. I wish Java had a TryParse method like C# does so we don't have to
+					//use try catch for flow control
+					
 				}
 				
-				//else, we don't know what it is
-				if (lowercaseQuery.equals("green"))
-				{
-					Toast.makeText(this, "Sorry, green line information isn't available yet", Toast.LENGTH_LONG).show();
-				}
-				else
-				{
-					Toast.makeText(this, "Route " + query + " doesn't exist", Toast.LENGTH_LONG).show();
-				}
+				//Toast.makeText(this, "Route " + query + " doesn't exist", Toast.LENGTH_LONG).show();
+				return IS_NOTHING;
 			}
 		}
 	}
