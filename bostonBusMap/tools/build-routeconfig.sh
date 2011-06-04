0f1ee6a1597c6d740096b16e1295b73a06b5eec4
George Schneeloch <bostonbusmap@gmail.com>
2011-06-04 01:02:03 +0000

    updating for los angeles

--- bostonBusMap/tools/build-routeconfig.sh
+++ bostonBusMap/tools/build-routeconfig.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-wget "http://webservices.nextbus.com/service/publicXMLFeed?a=mbta&command=routeList" -O routeList
+wget "http://webservices.nextbus.com/service/publicXMLFeed?a=lametro&command=routeList" -O routeList
 cat routeList | grep route | awk -F"\"" '{ print $2 }' > routes
 
-for each in `cat routes`; do wget "http://webservices.nextbus.com/service/publicXMLFeed?a=mbta&command=routeConfig&verbose&r=$each" -O routeConfig$each; sleep 10; done
+for each in `cat routes`; do wget "http://webservices.nextbus.com/service/publicXMLFeed?a=lametro&command=routeConfig&verbose&r=$each" -O routeConfig$each; sleep 10; done
 
 touch routeconfig_full.xml
 echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?> " >> routeconfig_full.xml
-echo "<body copyright=\"All data copyright MBTA 2011.\">" >> routeconfig_full.xml
+echo "<body copyright=\"All data copyright Los Angeles Metro 2011.\">" >> routeconfig_full.xml
 
 #for each in `cat routes`; do cat routeConfig$each | awk '$0 ~ /<body/ { next } $0 ~ /body>/ { next } $0 ~ /<\?xml/ { next } { print }' >> routeconfig_full.xml; done
 for each in `cat routes`; do cat routeConfig$each | awk 'BEGIN { in_path = 0 } $0 ~ /<body/ { next } $0 ~ /body>/ { next } $0 ~ /<\?xml/ { next } $0 ~ /<path/ { in_path=1; next; } $0 ~ /path>/ { in_path=0; next; } { if (!in_path) { print } }' >> routeconfig_full.xml; done
