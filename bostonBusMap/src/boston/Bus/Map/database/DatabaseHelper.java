3542f596eb4b7fe053142151fd5a04a605f852d9
George S <bostonbusmap@gmail.com>
2011-06-25 17:30:15 -0400

    upgrading before the current version will require refreshing all routes

--- bostonBusMap/src/boston/Bus/Map/database/DatabaseHelper.java
+++ bostonBusMap/src/boston/Bus/Map/database/DatabaseHelper.java
@@ -104,8 +104,9 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	public final static int VERBOSE_DB_5 = 17;
 	public final static int VERBOSE_DB_6 = 18;
 	public final static int VERBOSE_DB_7 = 19;
+	public final static int VERBOSE_DB_8 = 20;
 	
-	public final static int CURRENT_DB_VERSION = 20;
+	public final static int CURRENT_DB_VERSION = VERBOSE_DB_8;
 	
 	public static final int ALWAYS_POPULATE = 3;
 	public static final int POPULATE_IF_UPGRADE = 2;
@@ -159,7 +160,7 @@ public class DatabaseHelper extends SQLiteOpenHelper
 			populateFavorites(favorites, true, db);
 		}
 
-		if (oldVersion < VERBOSE_DB_3)
+		if (oldVersion < VERBOSE_DB_8)
 		{
 			db.execSQL("DROP TABLE IF EXISTS " + directionsTable);
 			db.execSQL("DROP TABLE IF EXISTS " + stopsTable);
