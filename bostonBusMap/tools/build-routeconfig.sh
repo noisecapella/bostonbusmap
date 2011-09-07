09acd1770d5e0d4ae8651dd11270212469da94bf
George Schneeloch <bostonbusmap@gmail.com>
2011-09-07 02:52:14 +0000

    force gzip

--- bostonBusMap/tools/build-routeconfig.sh
+++ bostonBusMap/tools/build-routeconfig.sh
@@ -17,4 +17,4 @@ for each in `cat routes`; do cat routeConfig$each | awk 'BEGIN { in_path = 0 } $
 
 echo "</body>" >> routeconfig_full.xml
 
-gzip routeconfig_full.xml
+gzip -f routeconfig_full.xml
