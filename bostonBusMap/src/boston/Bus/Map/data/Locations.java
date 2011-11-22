40bca128ce6f07a8f0383d62b8285ffffde9e8fa
bostonbusmap@gmail.com <bostonbusmap@gmail.com>
2011-11-22 16:59:27 -0500

    not refreshing commuter rail or subway for all predictions

--- bostonBusMap/src/boston/Bus/Map/data/Locations.java
+++ bostonBusMap/src/boston/Bus/Map/data/Locations.java
@@ -186,18 +186,28 @@ public final class Locations
 		
 		switch (selectedBusPredictions)
 		{
-		case Main.BUS_PREDICTIONS_ALL:
 		case Main.BUS_PREDICTIONS_STAR:
 		case Main.VEHICLE_LOCATIONS_ALL:
 			//get data from many transit sources
 			transitSystem.refreshData(routeConfig, selectedBusPredictions, maxStops, centerLatitude,
 					centerLongitude, busMapping, selectedRoute, routeMapping, directions, this);
 			break;
+		case Main.BUS_PREDICTIONS_ALL:
+		{
+			TransitSource transitSource = transitSystem.getTransitSource(null);
+			transitSource.refreshData(routeConfig, selectedBusPredictions, maxStops,
+					centerLatitude, centerLongitude, busMapping,
+					selectedRoute, routeMapping, directions, this);
+		}
+			break;
 		default:
+		{
 			TransitSource transitSource = routeConfig.getTransitSource();
 			transitSource.refreshData(routeConfig, selectedBusPredictions, maxStops,
 					centerLatitude, centerLongitude, busMapping,
 					selectedRoute, routeMapping, directions, this);
+		}
+			break;
 		}
 	}
 
