b3df322a2a2c4c8ca0b3fda0a116e987a1295aaf
George Schneeloch <bostonbusmap@gmail.com>
2012-04-14 07:03:45 -0400

    fixing current db version

--- src/boston/Bus/Map/database/DatabaseHelper.java
+++ src/boston/Bus/Map/database/DatabaseHelper.java
@@ -120,7 +120,7 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	public final static int VERBOSE_DBV2_2 = 26;
 	public final static int VERBOSE_DBV2_3 = 27;
 	
-	public final static int CURRENT_DB_VERSION = VERBOSE_DBV2_2;
+	public final static int CURRENT_DB_VERSION = VERBOSE_DBV2_3;
 	
 	public static final int ALWAYS_POPULATE = 3;
 	public static final int POPULATE_IF_UPGRADE = 2;
