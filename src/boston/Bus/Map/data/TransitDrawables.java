1c01ecad4511993a87f7a420b13355cb32ee8690
George Schneeloch <bostonbusmap@gmail.com>
2013-03-03 13:47:49 -0500

    handle multiple states

--- src/boston/Bus/Map/data/TransitDrawables.java
+++ src/boston/Bus/Map/data/TransitDrawables.java
@@ -12,6 +12,7 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.util.SparseArray;
 
 /**
@@ -29,6 +30,10 @@ public class TransitDrawables {
 	private final Drawable vehicle;
 	
 	private final SparseArray<Drawable> vehicles = new SparseArray<Drawable>();
+	
+	private static final int[][] validStates = new int[][] {
+		new int[]{android.R.attr.state_focused}, new int[0]
+	};
 	
 	public TransitDrawables(Context context, Drawable stop, Drawable stopUpdated, Drawable vehicle,
 			Drawable arrow, int arrowTop, Drawable intersection) {
@@ -53,42 +58,47 @@ public class TransitDrawables {
 	private Drawable createBusDrawable(int heading) {
 		//NOTE: 0, 0 is the bottom center point of the bus icon
 		
-		Bitmap bitmap = Bitmap.createBitmap(vehicle.getIntrinsicWidth(),
-				vehicle.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-		Canvas canvas = new Canvas(bitmap);
-		//first draw the bus
-		vehicle.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-		vehicle.draw(canvas);
-		
-		//then draw arrow
-		if (arrow != null && heading != BusLocation.NO_HEADING)
-		{
-			//put the arrow in the bus window
-			
-			int arrowLeft = -(arrow.getIntrinsicWidth() / 4);
-			int arrowTop = -vehicle.getIntrinsicHeight() + arrowTopDiff;
-			
-			//NOTE: use integer division when possible. This code is frequently executed
-			int arrowWidth = (arrow.getIntrinsicWidth() * 6) / 10;  
-			int arrowHeight = (arrow.getIntrinsicHeight() * 6) / 10;
-			int arrowRight = arrowLeft + arrowWidth;
-			int arrowBottom = arrowTop + arrowHeight;
+		StateListDrawable stateListDrawable = new StateListDrawable();
+		for (int[] state : validStates) {
+			Bitmap bitmap = Bitmap.createBitmap(vehicle.getIntrinsicWidth(),
+					vehicle.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+			Canvas canvas = new Canvas(bitmap);
+			//first draw the bus
+			vehicle.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+			vehicle.draw(canvas);
 
-			arrow.setBounds(arrowLeft, arrowTop, arrowRight, arrowBottom);
+			//then draw arrow
+			if (arrow != null && heading != BusLocation.NO_HEADING)
+			{
+				//put the arrow in the bus window
 
-			canvas.save();
-			//set rotation pivot at the center of the arrow image
-			canvas.rotate(heading, arrowLeft + arrowWidth/2, arrowTop + arrowHeight / 2);
+				int arrowLeft = -(arrow.getIntrinsicWidth() / 4);
+				int arrowTop = -vehicle.getIntrinsicHeight() + arrowTopDiff;
 
-			Rect rect = arrow.getBounds();
-			arrow.draw(canvas);
-			arrow.setBounds(rect);
-			
-			canvas.restore();
+				//NOTE: use integer division when possible. This code is frequently executed
+				int arrowWidth = (arrow.getIntrinsicWidth() * 6) / 10;  
+				int arrowHeight = (arrow.getIntrinsicHeight() * 6) / 10;
+				int arrowRight = arrowLeft + arrowWidth;
+				int arrowBottom = arrowTop + arrowHeight;
 
+				arrow.setBounds(arrowLeft, arrowTop, arrowRight, arrowBottom);
+
+				canvas.save();
+				//set rotation pivot at the center of the arrow image
+				canvas.rotate(heading, arrowLeft + arrowWidth/2, arrowTop + arrowHeight / 2);
+
+				Rect rect = arrow.getBounds();
+				arrow.draw(canvas);
+				arrow.setBounds(rect);
+
+				canvas.restore();
+
+			}
+			BitmapDrawable bitmapDrawable = new BitmapDrawable(context.getResources(), bitmap);
+			stateListDrawable.addState(state, bitmapDrawable);
 		}
 		
-		return new BitmapDrawable(context.getResources(), bitmap);
+		return stateListDrawable;
 	}
 
 	public Drawable getStop() {
