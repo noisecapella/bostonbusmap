f41c84d6a69864801d7f8d374560f76cb77c3c63
George Schneeloch <bostonbusmap@gmail.com>
2013-03-04 00:28:11 -0500

    highlight button

--- src/boston/Bus/Map/tutorials/IntroTutorial.java
+++ src/boston/Bus/Map/tutorials/IntroTutorial.java
@@ -3,6 +3,11 @@ package boston.Bus.Map.tutorials;
 import java.util.List;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import boston.Bus.Map.R;
 
@@ -58,17 +63,28 @@ public class IntroTutorial {
 			
 			@Override
 			public void setup(Activity parent) {
-				
+				ImageButton button = (ImageButton) parent.findViewById(R.id.myLocationButton);
+				Drawable buttonDrawable = button.getBackground();
+				if (!(buttonDrawable instanceof LayerDrawable)) {
+					Drawable redBackground = parent.getResources().getDrawable(R.drawable.red_background);
+					Drawable[] layers = new Drawable[]{redBackground, buttonDrawable};
+					LayerDrawable layerDrawable = new LayerDrawable(layers);
+					
+					button.setBackgroundDrawable(layerDrawable);
+				}
 			}
 			
 			@Override
 			public void teardown(Activity parent) {
-				
+				ImageButton button = (ImageButton) parent.findViewById(R.id.myLocationButton);
+				Drawable buttonDrawable = button.getBackground();
+				if (buttonDrawable instanceof LayerDrawable) {
+					button.setBackgroundDrawable(((LayerDrawable) buttonDrawable).getDrawable(1));
+				}
 			}
 			
 		});
 		steps.add(new TutorialStep() {
-			
 			@Override
 			public int getTextResource() {
 				return R.string.tutorialStep4;
