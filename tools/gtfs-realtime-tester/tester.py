56b584a4dc2b9d8dd5424922fa42c6ff182056b5
George Schneeloch <bostonbusmap@gmail.com>
2013-03-31 11:14:33 -0400

    add test python script

---
+++ tools/gtfs-realtime-tester/tester.py
@@ -0,0 +1,21 @@
+import sys
+import gtfs_realtime_pb2
+
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.web.client import getPage
+
+@inlineCallbacks
+def main():
+
+    data = yield getPage("http://developer.mbta.com/lib/gtrtfs/Passages.pb")
+    update = gtfs_realtime_pb2.FeedMessage()
+    update.ParseFromString(data)
+
+    print update
+
+    reactor.stop()
+
+if __name__ == "__main__":
+    reactor.callLater(0, main)
+    reactor.run()
