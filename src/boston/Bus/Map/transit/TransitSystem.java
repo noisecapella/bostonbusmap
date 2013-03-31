1256d695241cc3442e11f8054cc079e0ae9ecae1
George Schneeloch <bostonbusmap@gmail.com>
2013-03-31 09:52:32 -0400

    change website

--- src/boston/Bus/Map/transit/TransitSystem.java
+++ src/boston/Bus/Map/transit/TransitSystem.java
@@ -46,7 +46,7 @@ public class TransitSystem implements ITransitSystem {
 	private static final double bostonLatitude = 42.3583333;
 	private static final double bostonLongitude = -71.0602778;
 	
-	private static final String website = "http://www.terribleinformation.org/george/bostonbusmap";
+	private static final String website = "http://www.georgeschneeloch.com/bostonbusmap";
 	
 	//these four variables cover a very wide area just in case
 	public static final double lowerLeftLat = 41.582579601430346;
