200ea63574c269eb070b05715bd0abf1674702c2
George Schneeloch <bostonbusmap@gmail.com>
2013-05-24 18:05:38 -0400

    Update to handle new GTFS tables

--- tools/autogenerate.sh
+++ tools/autogenerate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PROGNAME=$(basename $0)
+GTFS_TO_SCHEDULE_DIR=../../gtfs-to-schedule
 
 set -e
 echo "Generating schema..."
@@ -14,8 +15,14 @@ echo "Parsing bus data..."
 python tosql.py routeconfig.xml routeList 15 >> sql.dump
 echo "Parsing alert data..."
 python alerts_tosql.py routeList subwayRouteList commuterRailRouteList >> sql.dump
-echo "Calculating bound times..."
-python calculate_times.py gtfs/stop_times.txt gtfs/trips.txt gtfs/calendar.txt >> sql.dump
+echo "Extracting GTFS data into timetables..."
+rm -f $GTFS_TO_SCHEDULE_DIR/gtfs_out.sql
+pushd $GTFS_TO_SCHEDULE_DIR
+python make_database.py ../gtfs gtfs_out.sql
+popd
+cat $GTFS_TO_SCHEDULE_DIR/gtfs_out.sql >> sql.dump
+#echo "Calculating bound times..."
+#python calculate_times.py gtfs/stop_times.txt gtfs/trips.txt gtfs/calendar.txt >> sql.dump
 echo "Dumping into sqlite..."
 rm new.db* || true
 sqlite3 new.db < sql.dump
