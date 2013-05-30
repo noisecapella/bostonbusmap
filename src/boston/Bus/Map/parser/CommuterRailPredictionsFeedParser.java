ece85885405c66b1e19b80617cf3f2ad7f95be3b
George Schneeloch <bostonbusmap@gmail.com>
2013-05-30 15:40:56 -0400

    Change commuter rail parser to use json

--- src/boston/Bus/Map/parser/CommuterRailPredictionsFeedParser.java
+++ src/boston/Bus/Map/parser/CommuterRailPredictionsFeedParser.java
@@ -10,12 +10,19 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+
+import skylight1.opengl.files.QuickParseUtil;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import boston.Bus.Map.data.BusLocation;
 import boston.Bus.Map.data.CommuterRailPrediction;
@@ -36,11 +43,10 @@ public class CommuterRailPredictionsFeedParser
 	private final RouteConfig routeConfig;
 	private final Directions directions;
 
-	private final Map<String, Integer> indexes = Maps.newHashMap();
-
-	
 	private final ConcurrentHashMap<String, BusLocation> busMapping;
 	private final RouteTitles routeKeysToTitles;
+
+	private final Set<String> vehiclesToRemove;
 
 	public CommuterRailPredictionsFeedParser(RouteConfig routeConfig, Directions directions,
 			ConcurrentHashMap<String, BusLocation> busMapping, RouteTitles routeKeysToTitles)
@@ -50,6 +56,7 @@ public class CommuterRailPredictionsFeedParser
 		this.busMapping = busMapping;
 		this.routeKeysToTitles = routeKeysToTitles;
 
+		vehiclesToRemove = Sets.newHashSet(busMapping.keySet());
 	}
 
 	private void clearPredictions(String route) throws IOException
@@ -63,8 +70,114 @@ public class CommuterRailPredictionsFeedParser
 		}
 	}
 
+	private class PredictionStopLocationPair {
+		private final Prediction prediction;
+		private final StopLocation stopLocation;
+		
+		public PredictionStopLocationPair(Prediction prediction, StopLocation stopLocation) {
+			this.prediction = prediction;
+			this.stopLocation = stopLocation;
+		}
+	}
+
 	public void runParse(Reader data) throws IOException
 	{
-		TODO
+		BufferedReader bufferedReader = new BufferedReader(data, 2048);
+
+		JsonElement root = new JsonParser().parse(bufferedReader);
+		List<PredictionStopLocationPair> pairs = parseTree(root.getAsJsonObject());
+		
+		String route = routeConfig.getRouteName();
+		clearPredictions(route);
+
+		for (PredictionStopLocationPair pair : pairs) {
+			pair.stopLocation.addPrediction(pair.prediction);
+		}
+		
+		for (String vehicleId : vehiclesToRemove) {
+			busMapping.remove(vehicleId);
+		}
+	}
+
+	private List<PredictionStopLocationPair> parseTree(JsonObject root) {
+		List<PredictionStopLocationPair> pairs = Lists.newArrayList();
+		
+		String routeName = routeConfig.getRouteName();
+		String routeTitle = routeKeysToTitles.getTitle(routeName);
+		long nowMillis = System.currentTimeMillis();
+		long nowSeconds = nowMillis / 1000;
+		
+		JsonArray messages = root.get("Messages").getAsJsonArray();
+		for (JsonElement element : messages) {
+			JsonObject message = element.getAsJsonObject();
+			String dirTag = message.get("Destination").getAsString();
+			Direction direction = directions.getDirection(dirTag);
+			if (direction == null) {
+				direction = new Direction(dirTag, "", routeName,
+						true);
+				directions.add(dirTag, direction);
+			}
+			
+			// add vehicle if exists
+			
+			String vehicle = message.get("Vehicle").getAsString();
+			
+			// not related to GTFS trip
+			String trip = message.get("Trip").getAsString();
+			String timestampString = message.get("TimeStamp").getAsString();
+			long timestamp = Long.parseLong(timestampString);
+
+			String latenessString = message.get("Lateness").getAsString();
+			int lateness;
+			if (latenessString.length() != 0) {
+				lateness = QuickParseUtil.parseInteger(latenessString);
+			}
+			else
+			{
+				lateness = 0;
+			}
+			if (vehicle.length() != 0) {
+				String latitudeString = message.get("Latitude").getAsString();
+				String longitudeString = message.get("Longitude").getAsString();
+				String headingString = message.get("Heading").getAsString();
+				
+				if (longitudeString.length() != 0 && latitudeString.length() != 0) {
+					float lat = QuickParseUtil.parseFloat(latitudeString);
+					float lon = QuickParseUtil.parseFloat(longitudeString);
+
+					CommuterTrainLocation location = new CommuterTrainLocation(lat,
+							lon, trip, timestamp, timestamp,
+							headingString, true, dirTag, routeName, directions,
+							routeTitle);
+					busMapping.put(trip, location);
+					vehiclesToRemove.remove(trip);
+				}
+			}
+			
+			// handle predictions
+			String stopId = message.get("Stop").getAsString();
+			String scheduledString = message.get("Scheduled").getAsString();
+			long scheduled = Long.parseLong(scheduledString);
+			if (stopId.length() != 0 && scheduledString.length() != 0) {
+				StopLocation stop = routeConfig.getStop(stopId);
+				if (stop != null) {
+					int seconds = (int)(scheduled - nowSeconds);
+					int minutes = seconds / 60;
+					Prediction prediction = new Prediction(minutes,
+							trip, dirTag, routeName,
+							routeTitle, false,
+							lateness > 5*60, lateness);
+					PredictionStopLocationPair pair = new PredictionStopLocationPair(prediction,
+							stop);
+					pairs.add(pair);
+				}
+				else
+				{
+					LogUtil.w("Commuter rail stop missing: " + stopId);
+				}
+			}
+		}
+		
+		return pairs;
 	}
 }
