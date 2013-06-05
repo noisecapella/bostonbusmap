702898c2a667de9904a703a931a9a99160375fe5
George Schneeloch <bostonbusmap@gmail.com>
2013-06-05 02:52:38 -0400

    Trying to fix problems with route path connections

--- tools/commuterrail_paths.py
+++ tools/commuterrail_paths.py
@@ -2,6 +2,8 @@ import os
 import argparse
 from gtfs_map import GtfsMap
 
+from collections import defaultdict
+
 def calculate_path(route, gtfs_map):
     trips_header, trips = gtfs_map.trips_header, gtfs_map.trips
     stops_header, stops = gtfs_map.stops_header, gtfs_map.stops
@@ -35,23 +37,54 @@ def calculate_path(route, gtfs_map):
         
         sequences[trip_id] = [(row[stop_times_header["stop_id"]], int(row[stop_times_header["stop_sequence"]])) for row in stop_times_rows]
 
+    # create a sequence map so we can connect trivial broken sequences
+    # (stop, sequence) -> [(next_stop, next_sequence)]
+    sequence_map = defaultdict(set)
+    for trip_id, sequence in sequences.items():
+        prev_tup = None
+        for tup in sequence:
+            if prev_tup is not None:
+                sequence_map[prev_tup].add(tup)
+            prev_tup = tup
+
+    def connectable(prev_tup, next_tup):
+        if prev_tup in sequence_map:
+            tup_set = sequence_map[prev_tup]
+            for missing_tup in tup_set:
+                if missing_tup in sequence_map:
+                    if next_tup in sequence_map[missing_tup]:
+                        if abs(prev_tup[1] - next_tup[1]) == 2:
+                            #print "Connection: %s %s %s" % (prev_tup, missing_tup, next_tup)
+                            return missing_tup
+
+        return None
+                
+
     # a list of lat, lon pairs
     paths = []
     for trip_id, sequence in sequences.items():
         #stops_path = [stops_to_lat_lon[stop] for stop in sequence]
         stops_path = []
-        prev_sequence_num = None
         # break up sequence into contiguous pieces. If two stops are not
         # contiguous we should not draw a line connecting the stops
-        for stop, sequence_num in sequence:
-            if prev_sequence_num is not None and prev_sequence_num != sequence_num - 1:
-                #print "Break: %s, %s, %d, %d" % (trip_id, stop, prev_sequence_num, sequence_num)
-                if len(stops_path) > 1:
-                    paths.append(stops_path)
-                stops_path = []
+
+        prev_tup = None
+        for tup in sequence:
+            stop, sequence_num = tup
+            if prev_tup is not None and prev_tup[1] != sequence_num - 1:
+                # check if we can easily connect with the prev and next items
+                missing_tup = connectable(prev_tup, tup)
+                if missing_tup is not None:
+                    missing_stop, _ = missing_tup
+                    stops_path.append(stops_to_lat_lon[missing_stop])
+                    prev_tup = missing_tup
+                else:
+                    # stop and break into new path
+                    if len(stops_path) > 1:
+                        paths.append(stops_path)
+                    stops_path = []
             stops_path.append(stops_to_lat_lon[stop])
-            prev_sequence_num = sequence_num
-                
+            prev_tup = tup
         paths.append(stops_path)
     return paths
 
