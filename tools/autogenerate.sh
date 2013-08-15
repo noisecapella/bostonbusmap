a3e73f0cc8fb0231c163ed7b7896fa5172a00e91
George Schneeloch <bostonbusmap@gmail.com>
2013-08-14 23:32:09 -0400

    Fix merge error

--- tools/autogenerate.sh
+++ tools/autogenerate.sh
@@ -13,8 +13,6 @@ echo "Parsing subway data..."
 python heavyrail_tosql.py ../../gtfs 12 >> sql.dump
 echo "Parsing bus data..."
 python tosql.py routeconfig.xml routeList 15 >> sql.dump
-echo "Parsing alert data..."
-python alerts_tosql.py routeList subwayRouteList commuterRailRouteList >> sql.dump
 echo "Extracting GTFS data into timetables..."
 rm -f $GTFS_TO_SCHEDULE_DIR/gtfs_out.sql
 pushd $GTFS_TO_SCHEDULE_DIR
