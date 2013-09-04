3ba08a19c634e905fea20686c0df007e1ac8371c
George Schneeloch <bostonbusmap@gmail.com>
2013-09-04 07:16:03 -0400

    Cleanup

--- tools/build-routeconfig.sh
+++ tools/build-routeconfig.sh
@@ -7,14 +7,26 @@ echo "<body copyright=\"All data copyright $agency 2011.\">" >> routeconfig_full
 for agency in brooklyn bronx staten-island
 do
 
-    wget "http://$prefix.nextbus.com/service/publicXMLFeed?a=$agency&command=routeList" -O routeList_$agency
+    wget "http://$prefix.nextbus.com/service/publicXMLFeed?a=$agency&command=routeList" -O routeList_$agency --quiet
     cat routeList_$agency | grep route | awk -F"\"" '{ print $2 }' > routes_$agency
 
-    for each in `cat routes_$agency`; do wget "http://$prefix.nextbus.com/service/publicXMLFeed?a=$agency&command=routeConfig&verbose&r=$each" -O routeConfig$each; sleep 10; done
+    for each in `cat routes_$agency`
+    do
+	if wget "http://$prefix.nextbus.com/service/publicXMLFeed?a=$agency&command=routeConfig&verbose&r=$each" -O routeConfig$each --quiet
+	then
+	    echo "Downloaded $each"
+	else
+	    echo "Failed to download $each"
+	    sleep 10
+	fi
+    done
 
     touch routeconfig_full.xml
 
-    for each in `cat routes_$agency`; do cat routeConfig$each | awk 'BEGIN { in_path = 0 } $0 ~ /<body/ { next } $0 ~ /body>/ { next } $0 ~ /<\?xml/ { next } { print }' >> routeconfig_full.xml; done
+    for each in `cat routes_$agency`
+    do 
+	cat routeConfig$each | awk 'BEGIN { in_path = 0 } $0 ~ /<body/ { next } $0 ~ /body>/ { next } $0 ~ /<\?xml/ { next } { print }' >> routeconfig_full.xml
+    done
     
 done
 echo "</body>" >> routeconfig_full.xml
