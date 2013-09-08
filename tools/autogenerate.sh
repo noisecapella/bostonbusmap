f28ebee750879dfa7b679d6ac2ffbf0f9de507cf
George Schneeloch <bostonbusmap@gmail.com>
2013-09-08 12:19:32 -0400

    Change autogenerate.sh to use generate/ python scripts. Remove old files

--- tools/autogenerate.sh
+++ tools/autogenerate.sh
@@ -3,22 +3,24 @@ PROGNAME=$(basename $0)
 GTFS_DIR=gtfs/mbta
 
 set -e
-echo "Generating schema..."
+echo "Generate Schema.java..."
 python generate_schema.py > ../src/boston/Bus/Map/database/Schema.java
-echo "Create tables..."
-python create_tables.py > sql.dump
-echo "Parsing Hubway data..."
-python hubway_tosql.py 0 >> sql.dump
-echo "Parsing commuter rail data..."
-python commuterrail_tosql.py "$GTFS_DIR" 1 StationOrder.csv >> sql.dump
-echo "Parsing subway data..."
-python heavyrail_tosql.py "$GTFS_DIR" 13 >> sql.dump
-echo "Parsing bus data..."
-python tosql.py routeconfig.xml routeList 16 >> sql.dump
-echo "Dumping into sqlite..."
-rm new.db* || true
-sqlite3 new.db < sql.dump
-gzip new.db
-cp new.db.gz ../res/raw/databasegz
+echo "Generating database..."
+(cd generate; python generate_database.py ../new.db)
+#echo "Create tables..."
+#python create_tables.py > sql.dump
+#echo "Parsing Hubway data..."
+#python hubway_tosql.py 0 >> sql.dump
+#echo "Parsing commuter rail data..."
+#python commuterrail_tosql.py "$GTFS_DIR" 1 StationOrder.csv >> sql.dump
+#echo "Parsing subway data..."
+#python heavyrail_tosql.py "$GTFS_DIR" 13 >> sql.dump
+#echo "Parsing bus data..."
+#python tosql.py routeconfig.xml routeList 16 >> sql.dump
+#echo "Dumping into sqlite..."
+#rm new.db* || true
+#sqlite3 new.db < sql.dump
+#gzip new.db
+#cp new.db.gz ../res/raw/databasegz
 echo "Done!"
 
--- tools/generate/generate_database.py
+++ tools/generate/generate_database.py
@@ -7,30 +7,42 @@ from create_tables import create_tables
 
 import os
 import argparse
+import traceback
 
 @inlineCallbacks
 def generate(conn):
     create_tables(conn)
+    print "Generating Hubway stops..."
     index = yield Hubway().generate(conn, 0)
+    print "Generating commuter rail stops..."
     index = yield MbtaCommuterRail().generate(conn, index)
+    print "Generating heavy rail stops..."
     index = yield MbtaHeavyRail().generate(conn, index)
+    print "Generating NextBus stops..."
     index = yield NextBus("mbta").generate(conn, index)
     print index
 
 @inlineCallbacks
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("output_database")
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("output_database")
+        args = parser.parse_args()
 
-    os.unlink(args.output_database)
+        try:
+            os.unlink(args.output_database)
+        except:
+            pass
 
-    conn = sqlite3.connect(args.output_database)
-    yield generate(conn)
+        conn = sqlite3.connect(args.output_database)
+        yield generate(conn)
 
-    conn.close()
-
-    reactor.stop()
+        conn.close()
+        
+        reactor.stop()
+    except Exception as e:
+        reactor.stop()
+        raise
 
 
 if __name__ == "__main__":
