f2eba71e772cfcc4d9e883c8007bebcecb0170ab
George Schneeloch <bostonbusmap@gmail.com>
2014-07-27 17:08:54 -0400

    Inc version

--- src/boston/Bus/Map/provider/DatabaseContentProvider.java
+++ src/boston/Bus/Map/provider/DatabaseContentProvider.java
@@ -176,7 +176,8 @@ public class DatabaseContentProvider extends ContentProvider {
 	public final static int FIX_LOCATIONS_6 = 46;
 	public final static int FIX_LOCATIONS_7 = 47;
 	public final static int FIX_LOCATIONS_8 = 48;
-	public final static int CURRENT_DB_VERSION = FIX_LOCATIONS_8;
+	public final static int FIX_LOCATIONS_9 = 49;
+	public final static int CURRENT_DB_VERSION = FIX_LOCATIONS_9;
 
 	public static final int ALWAYS_POPULATE = 3;
 	public static final int POPULATE_IF_UPGRADE = 2;
