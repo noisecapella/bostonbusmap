f0dfc12e2e0da1f0054fb132c985ffaa64e40857
George Schneeloch <noisecapella@gmail.com>
2015-01-02 11:34:22 -0500

    Add BusDrawables to manage the new bus icons

---
+++ src/boston/Bus/Map/ui/BusDrawables.java
@@ -0,0 +1,193 @@
+package boston.Bus.Map.ui;
+
+import boston.Bus.Map.R;
+
+public class BusDrawables {
+
+    public static int getIdFromAngle(int angle) {
+
+        if (angle < 8) {
+            return R.drawable.bus_0;
+        }
+
+        else if (angle < 16) {
+            return R.drawable.bus_8;
+        }
+
+        else if (angle < 24) {
+            return R.drawable.bus_16;
+        }
+
+        else if (angle < 32) {
+            return R.drawable.bus_24;
+        }
+
+        else if (angle < 40) {
+            return R.drawable.bus_32;
+        }
+
+        else if (angle < 48) {
+            return R.drawable.bus_40;
+        }
+
+        else if (angle < 56) {
+            return R.drawable.bus_48;
+        }
+
+        else if (angle < 64) {
+            return R.drawable.bus_56;
+        }
+
+        else if (angle < 72) {
+            return R.drawable.bus_64;
+        }
+
+        else if (angle < 80) {
+            return R.drawable.bus_72;
+        }
+
+        else if (angle < 88) {
+            return R.drawable.bus_80;
+        }
+
+        else if (angle < 96) {
+            return R.drawable.bus_88;
+        }
+
+        else if (angle < 104) {
+            return R.drawable.bus_96;
+        }
+
+        else if (angle < 112) {
+            return R.drawable.bus_104;
+        }
+
+        else if (angle < 120) {
+            return R.drawable.bus_112;
+        }
+
+        else if (angle < 128) {
+            return R.drawable.bus_120;
+        }
+
+        else if (angle < 136) {
+            return R.drawable.bus_128;
+        }
+
+        else if (angle < 144) {
+            return R.drawable.bus_136;
+        }
+
+        else if (angle < 152) {
+            return R.drawable.bus_144;
+        }
+
+        else if (angle < 160) {
+            return R.drawable.bus_152;
+        }
+
+        else if (angle < 168) {
+            return R.drawable.bus_160;
+        }
+
+        else if (angle < 176) {
+            return R.drawable.bus_168;
+        }
+
+        else if (angle < 184) {
+            return R.drawable.bus_176;
+        }
+
+        else if (angle < 192) {
+            return R.drawable.bus_184;
+        }
+
+        else if (angle < 200) {
+            return R.drawable.bus_192;
+        }
+
+        else if (angle < 208) {
+            return R.drawable.bus_200;
+        }
+
+        else if (angle < 216) {
+            return R.drawable.bus_208;
+        }
+
+        else if (angle < 224) {
+            return R.drawable.bus_216;
+        }
+
+        else if (angle < 232) {
+            return R.drawable.bus_224;
+        }
+
+        else if (angle < 240) {
+            return R.drawable.bus_232;
+        }
+
+        else if (angle < 248) {
+            return R.drawable.bus_240;
+        }
+
+        else if (angle < 256) {
+            return R.drawable.bus_248;
+        }
+
+        else if (angle < 264) {
+            return R.drawable.bus_256;
+        }
+
+        else if (angle < 272) {
+            return R.drawable.bus_264;
+        }
+
+        else if (angle < 280) {
+            return R.drawable.bus_272;
+        }
+
+        else if (angle < 288) {
+            return R.drawable.bus_280;
+        }
+
+        else if (angle < 296) {
+            return R.drawable.bus_288;
+        }
+
+        else if (angle < 304) {
+            return R.drawable.bus_296;
+        }
+
+        else if (angle < 312) {
+            return R.drawable.bus_304;
+        }
+
+        else if (angle < 320) {
+            return R.drawable.bus_312;
+        }
+
+        else if (angle < 328) {
+            return R.drawable.bus_320;
+        }
+
+        else if (angle < 336) {
+            return R.drawable.bus_328;
+        }
+
+        else if (angle < 344) {
+            return R.drawable.bus_336;
+        }
+
+        else if (angle < 352) {
+            return R.drawable.bus_344;
+        }
+
+        else if (angle < 360) {
+            return R.drawable.bus_352;
+        }
+
+        else {
+            return R.drawable.bus_0;
+        }
+    }
+}
