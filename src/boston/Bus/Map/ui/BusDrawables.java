a24da4171d001e173f4fdb1caf3bfd2886dbf5bd
George Schneeloch <noisecapella@gmail.com>
2015-01-13 00:55:43 -0500

    Update drawables

--- src/boston/Bus/Map/ui/BusDrawables.java
+++ src/boston/Bus/Map/ui/BusDrawables.java
@@ -4,7 +4,7 @@ import boston.Bus.Map.R;
 
 public class BusDrawables {
 
-    private static final int[] idLookup = new int[] {
+    public static final int[] busLookup = new int[] {
          R.drawable.bus_0,
         R.drawable.bus_8,
         R.drawable.bus_16,
@@ -52,7 +52,7 @@ public class BusDrawables {
         R.drawable.bus_352
         };
 
-    private static final int[] idSelectedLookup = new int[] {
+    public static final int[] bus_selectedLookup = new int[] {
          R.drawable.bus_statelist_0,
         R.drawable.bus_statelist_8,
         R.drawable.bus_statelist_16,
@@ -99,14 +99,101 @@ public class BusDrawables {
         R.drawable.bus_statelist_344,
         R.drawable.bus_statelist_352
         };
-    
 
-    public static int getIdFromAngle(int angle, boolean isSelected) {
-        angle += (360 / idSelectedLookup.length) / 2;
-        angle = angle % 360;
-        if (angle < 0 || angle >= 360) {
-            return R.drawable.bus_statelist_0;
-        }
-        return idSelectedLookup[angle/8];
-    }
+    public static final int[] railLookup = new int[] {
+         R.drawable.rail_0,
+        R.drawable.rail_8,
+        R.drawable.rail_16,
+        R.drawable.rail_24,
+        R.drawable.rail_32,
+        R.drawable.rail_40,
+        R.drawable.rail_48,
+        R.drawable.rail_56,
+        R.drawable.rail_64,
+        R.drawable.rail_72,
+        R.drawable.rail_80,
+        R.drawable.rail_88,
+        R.drawable.rail_96,
+        R.drawable.rail_104,
+        R.drawable.rail_112,
+        R.drawable.rail_120,
+        R.drawable.rail_128,
+        R.drawable.rail_136,
+        R.drawable.rail_144,
+        R.drawable.rail_152,
+        R.drawable.rail_160,
+        R.drawable.rail_168,
+        R.drawable.rail_176,
+        R.drawable.rail_184,
+        R.drawable.rail_192,
+        R.drawable.rail_200,
+        R.drawable.rail_208,
+        R.drawable.rail_216,
+        R.drawable.rail_224,
+        R.drawable.rail_232,
+        R.drawable.rail_240,
+        R.drawable.rail_248,
+        R.drawable.rail_256,
+        R.drawable.rail_264,
+        R.drawable.rail_272,
+        R.drawable.rail_280,
+        R.drawable.rail_288,
+        R.drawable.rail_296,
+        R.drawable.rail_304,
+        R.drawable.rail_312,
+        R.drawable.rail_320,
+        R.drawable.rail_328,
+        R.drawable.rail_336,
+        R.drawable.rail_344,
+        R.drawable.rail_352
+        };
+
+    public static final int[] rail_selectedLookup = new int[] {
+         R.drawable.rail_statelist_0,
+        R.drawable.rail_statelist_8,
+        R.drawable.rail_statelist_16,
+        R.drawable.rail_statelist_24,
+        R.drawable.rail_statelist_32,
+        R.drawable.rail_statelist_40,
+        R.drawable.rail_statelist_48,
+        R.drawable.rail_statelist_56,
+        R.drawable.rail_statelist_64,
+        R.drawable.rail_statelist_72,
+        R.drawable.rail_statelist_80,
+        R.drawable.rail_statelist_88,
+        R.drawable.rail_statelist_96,
+        R.drawable.rail_statelist_104,
+        R.drawable.rail_statelist_112,
+        R.drawable.rail_statelist_120,
+        R.drawable.rail_statelist_128,
+        R.drawable.rail_statelist_136,
+        R.drawable.rail_statelist_144,
+        R.drawable.rail_statelist_152,
+        R.drawable.rail_statelist_160,
+        R.drawable.rail_statelist_168,
+        R.drawable.rail_statelist_176,
+        R.drawable.rail_statelist_184,
+        R.drawable.rail_statelist_192,
+        R.drawable.rail_statelist_200,
+        R.drawable.rail_statelist_208,
+        R.drawable.rail_statelist_216,
+        R.drawable.rail_statelist_224,
+        R.drawable.rail_statelist_232,
+        R.drawable.rail_statelist_240,
+        R.drawable.rail_statelist_248,
+        R.drawable.rail_statelist_256,
+        R.drawable.rail_statelist_264,
+        R.drawable.rail_statelist_272,
+        R.drawable.rail_statelist_280,
+        R.drawable.rail_statelist_288,
+        R.drawable.rail_statelist_296,
+        R.drawable.rail_statelist_304,
+        R.drawable.rail_statelist_312,
+        R.drawable.rail_statelist_320,
+        R.drawable.rail_statelist_328,
+        R.drawable.rail_statelist_336,
+        R.drawable.rail_statelist_344,
+        R.drawable.rail_statelist_352
+        };
+
 }
