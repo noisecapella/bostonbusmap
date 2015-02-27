cc01a11ed1b7c5a6c2bcbf0b30e32e1b2092c286
George Schneeloch <bostonbusmap@gmail.com>
2015-02-27 01:40:30 -0500

    Add back old commuter rail feed for now

---
+++ bostonbusmap-library/src/main/java/com/schneeloch/bostonbusmap_library/parser/CommuterRailPredictionsFeedParser.java
@@ -0,0 +1,167 @@
+package com.schneeloch.bostonbusmap_library.parser;
+
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.schneeloch.bostonbusmap_library.data.BusLocation;
+import com.schneeloch.bostonbusmap_library.data.CommuterRailPrediction;
+import com.schneeloch.bostonbusmap_library.data.CommuterTrainLocation;
+import com.schneeloch.bostonbusmap_library.data.Direction;
+import com.schneeloch.bostonbusmap_library.data.Directions;
+import com.schneeloch.bostonbusmap_library.data.PredictionStopLocationPair;
+import com.schneeloch.bostonbusmap_library.data.RouteConfig;
+import com.schneeloch.bostonbusmap_library.data.RouteTitles;
+import com.schneeloch.bostonbusmap_library.data.StopLocation;
+import com.schneeloch.bostonbusmap_library.data.VehicleLocations;
+import com.schneeloch.bostonbusmap_library.database.Schema;
+import com.schneeloch.bostonbusmap_library.util.LogUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CommuterRailPredictionsFeedParser
+{
+	private final RouteConfig routeConfig;
+	private final Directions directions;
+
+	private final VehicleLocations busMapping;
+	private final RouteTitles routeKeysToTitles;
+
+	/**
+	 * Keep this value consistent while reading data
+	 */
+	private final long currentTimeMillis;
+
+	public CommuterRailPredictionsFeedParser(RouteConfig routeConfig, Directions directions,
+			VehicleLocations busMapping, RouteTitles routeKeysToTitles)
+	{
+		this.routeConfig = routeConfig;
+		this.directions = directions;
+		this.busMapping = busMapping;
+		this.routeKeysToTitles = routeKeysToTitles;
+
+		this.currentTimeMillis = System.currentTimeMillis();
+	}
+
+	private void clearPredictions() throws IOException
+	{
+		for (StopLocation stopLocation : routeConfig.getStops())
+		{
+			stopLocation.clearPredictions(routeConfig);
+		}
+	}
+
+	public void runParse(Reader data) throws IOException
+	{
+		BufferedReader bufferedReader = new BufferedReader(data, 2048);
+
+		JsonElement root = new JsonParser().parse(bufferedReader);
+		List<PredictionStopLocationPair> pairs = parseTree(root.getAsJsonObject());
+		
+		clearPredictions();
+
+		for (PredictionStopLocationPair pair : pairs) {
+			pair.stopLocation.addPrediction(pair.prediction);
+		}
+	}
+
+	private List<PredictionStopLocationPair> parseTree(JsonObject root) {
+		List<PredictionStopLocationPair> pairs = Lists.newArrayList();
+		
+		String routeName = routeConfig.getRouteName();
+		String routeTitle = routeKeysToTitles.getTitle(routeName);
+		long nowMillis = System.currentTimeMillis();
+		long nowSeconds = nowMillis / 1000;
+
+        Map<VehicleLocations.Key, BusLocation> newLocations = Maps.newHashMap();
+        JsonArray messages = root.get("Messages").getAsJsonArray();
+		for (JsonElement element : messages) {
+			JsonObject message = element.getAsJsonObject();
+			String dirTag = message.get("Destination").getAsString();
+			Direction direction = directions.getDirection(dirTag);
+			if (direction == null) {
+				direction = new Direction(dirTag, "", routeName,
+						true);
+				directions.add(dirTag, direction);
+			}
+			
+			// add vehicle if exists
+			
+			String vehicle = message.get("Vehicle").getAsString();
+			
+			// not related to GTFS trip
+			String trip = message.get("Trip").getAsString();
+			String timestampString = message.get("TimeStamp").getAsString();
+			long timestamp = Long.parseLong(timestampString);
+			long timestampMillis = timestamp * 1000;
+
+			String latenessString = message.get("Lateness").getAsString();
+			int lateness;
+			if (latenessString.length() != 0) {
+				lateness = Integer.parseInt(latenessString);
+			}
+			else
+			{
+				lateness = 0;
+			}
+			if (vehicle.length() != 0) {
+				String latitudeString = message.get("Latitude").getAsString();
+				String longitudeString = message.get("Longitude").getAsString();
+				String headingString = message.get("Heading").getAsString();
+				
+				if (longitudeString.length() != 0 && latitudeString.length() != 0) {
+					float lat = Float.parseFloat(latitudeString);
+					float lon = Float.parseFloat(longitudeString);
+
+					CommuterTrainLocation location = new CommuterTrainLocation(lat,
+							lon, trip, timestampMillis, timestampMillis,
+							headingString, true, dirTag, routeName, directions,
+							routeTitle);
+                    newLocations.put(new VehicleLocations.Key(Schema.Routes.enumagencyidCommuterRail, routeName, trip), location);
+				}
+			}
+			
+			// handle predictions
+			String flag = message.get("Flag").getAsString();
+			String stopId = message.get("Stop").getAsString();
+			String scheduledString = message.get("Scheduled").getAsString();
+			long scheduled = Long.parseLong(scheduledString);
+			if (stopId.length() != 0 && scheduledString.length() != 0) {
+				StopLocation stop = routeConfig.getStop(stopId);
+				if (stop != null) {
+					int seconds = (int)(scheduled - nowSeconds);
+					int minutes = seconds / 60;
+					long arrivalTimeMillis = currentTimeMillis + minutes * 60 * 1000;
+
+
+					CommuterRailPrediction prediction = new CommuterRailPrediction(arrivalTimeMillis,
+							trip, dirTag, routeName,
+							routeTitle, false,
+							lateness > 5*60, lateness, "", stopId,
+							CommuterRailPrediction.Flag.toFlagEnum(flag)
+							);
+					PredictionStopLocationPair pair = new PredictionStopLocationPair(prediction,
+							stop);
+					pairs.add(pair);
+				}
+				else
+				{
+					LogUtil.w("Commuter rail stop missing: " + stopId);
+				}
+			}
+		}
+        busMapping.update(Schema.Routes.enumagencyidCommuterRail, ImmutableSet.of(routeName), false, newLocations);
+
+		return pairs;
+	}
+}
---
+++ bostonbusmap-library/src/main/java/com/schneeloch/bostonbusmap_library/transit/CommuterRailTransitSource.java
@@ -0,0 +1,325 @@
+package com.schneeloch.bostonbusmap_library.transit;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.schneeloch.bostonbusmap_library.data.BusLocation;
+import com.schneeloch.bostonbusmap_library.data.CommuterRailStopLocation;
+import com.schneeloch.bostonbusmap_library.data.Directions;
+import com.schneeloch.bostonbusmap_library.data.IAlerts;
+import com.schneeloch.bostonbusmap_library.data.ITransitDrawables;
+import com.schneeloch.bostonbusmap_library.data.Location;
+import com.schneeloch.bostonbusmap_library.data.Locations;
+import com.schneeloch.bostonbusmap_library.data.RouteConfig;
+import com.schneeloch.bostonbusmap_library.data.RoutePool;
+import com.schneeloch.bostonbusmap_library.data.Selection;
+import com.schneeloch.bostonbusmap_library.data.StopLocation;
+import com.schneeloch.bostonbusmap_library.data.TransitSourceTitles;
+import com.schneeloch.bostonbusmap_library.data.VehicleLocations;
+import com.schneeloch.bostonbusmap_library.database.Schema;
+import com.schneeloch.bostonbusmap_library.parser.CommuterRailPredictionsFeedParser;
+import com.schneeloch.bostonbusmap_library.util.DownloadHelper;
+import com.schneeloch.bostonbusmap_library.util.FeedException;
+import com.schneeloch.bostonbusmap_library.util.LogUtil;
+import com.schneeloch.bostonbusmap_library.util.SearchHelper;
+
+import org.xml.sax.SAXException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+public class CommuterRailTransitSource implements TransitSource {
+	private final ITransitDrawables drawables;
+	private final TransitSourceTitles routeTitles;
+	private final TransitSystem transitSystem;
+	
+	public static final int COLOR = 0x940088;
+	
+	private final ImmutableMap<String, String> routesToUrls;
+	
+	public CommuterRailTransitSource(ITransitDrawables drawables,
+			TransitSourceTitles routeTitles,
+			TransitSystem transitSystem)
+	{
+		this.drawables = drawables;
+		this.routeTitles = routeTitles;
+		this.transitSystem = transitSystem;
+		
+		final String predictionsUrlSuffix = ".json";
+		final String dataUrlPrefix = "http://developer.mbta.com/lib/RTCR/RailLine_";
+		
+		ImmutableMap.Builder<String, String> urlBuilder = ImmutableMap.builder();
+		urlBuilder.put("CR-Greenbush", dataUrlPrefix + 1 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Kingston", dataUrlPrefix + 2 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Middleborough", dataUrlPrefix + 3 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Fairmount", dataUrlPrefix + 4 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Providence", dataUrlPrefix + 5 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Franklin", dataUrlPrefix + 6 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Needham", dataUrlPrefix + 7 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Worcester", dataUrlPrefix + 8 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Fitchburg", dataUrlPrefix + 9 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Lowell", dataUrlPrefix + 10 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Haverhill", dataUrlPrefix + 11 + predictionsUrlSuffix);
+		urlBuilder.put("CR-Newburyport", dataUrlPrefix + 12 + predictionsUrlSuffix);
+		routesToUrls = urlBuilder.build();
+	}
+
+	@Override
+	public void refreshData(RouteConfig routeConfig,
+			Selection selection, int maxStops, double centerLatitude,
+			double centerLongitude,
+			final VehicleLocations busMapping,
+			final RoutePool routePool, final Directions directions,
+			final Locations locationsObj) throws IOException,
+            ParserConfigurationException, SAXException
+	{
+		Selection.Mode selectedBusPredictions = selection.getMode();
+
+		List<RefreshData> outputData = Lists.newArrayList();
+        switch (selectedBusPredictions) {
+            case BUS_PREDICTIONS_ONE:
+            case VEHICLE_LOCATIONS_ONE: {
+                List<Location> locations = locationsObj.getLocations(maxStops, centerLatitude, centerLongitude, false, selection);
+
+                //ok, do predictions now
+                getPredictionsUrl(locations, maxStops, routeConfig.getRouteName(), outputData, selectedBusPredictions);
+                break;
+            }
+            case BUS_PREDICTIONS_ALL:
+            case VEHICLE_LOCATIONS_ALL:
+            case BUS_PREDICTIONS_STAR: {
+                List<Location> locations = locationsObj.getLocations(maxStops, centerLatitude, centerLongitude, false, selection);
+
+                getPredictionsUrl(locations, maxStops, null, outputData, selectedBusPredictions);
+
+                break;
+            }
+            default:
+                throw new RuntimeException("Unexpected mode");
+        }
+
+        // split into up to 12 threads
+        final ConcurrentHashMap<String, Exception> exceptions = new ConcurrentHashMap<>();
+        List<Thread> threads = Lists.newArrayList();
+        for (final RefreshData outputRow : outputData)
+        {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        String url = outputRow.url;
+                        DownloadHelper downloadHelper = new DownloadHelper(url);
+
+                        downloadHelper.connect();
+
+
+
+                        InputStream stream = downloadHelper.getResponseData();
+                        InputStreamReader data = new InputStreamReader(stream);
+                        //StringReader data = new StringReader(hardcodedData);
+
+                        //bus prediction
+
+                        String route = outputRow.route;
+                        RouteConfig railRouteConfig = routePool.get(route);
+                        CommuterRailPredictionsFeedParser parser = new CommuterRailPredictionsFeedParser(railRouteConfig, directions,
+                                busMapping, locationsObj.getRouteTitles());
+
+                        parser.runParse(data);
+                        data.close();
+                    } catch (Exception e) {
+                        exceptions.put(outputRow.route, e);
+                    }
+                }
+            });
+            threads.add(thread);
+            thread.start();
+        }
+
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                LogUtil.e(e);
+            }
+        }
+        if (exceptions.size() > 0) {
+            // hopefully no more than one. We can't throw more than one exception at a time
+            String key = exceptions.keySet().iterator().next();
+            Exception exception = exceptions.get(key);
+            throw new FeedException("Error downloading from commuter rail route " + key + " data feed", exception);
+        }
+	}
+
+	private static class RefreshData {
+		private final String url;
+		private final String route;
+		
+		public RefreshData(String url, String route) {
+			this.url = url;
+			this.route = route;
+		}
+	}
+	
+	private void getPredictionsUrl(List<Location> locations, int maxStops,
+			String routeName, List<RefreshData> outputData, Selection.Mode mode)
+	{
+		//http://developer.mbta.com/lib/RTCR/RailLine_1.csv
+		
+		//BUS_PREDICTIONS_ONE or VEHICLE_LOCATIONS_ONE
+		if (routeName != null)
+		{
+			//we know we're updating only one route
+			if (isCommuterRail(routeName))
+			{
+				
+				String url = routesToUrls.get(routeName);
+				
+				outputData.add(new RefreshData(url, routeName));
+				return;
+			}
+		}
+		else
+		{
+			if (mode == Selection.Mode.BUS_PREDICTIONS_STAR)
+			{
+				//ok, let's look at the locations and see what we can get
+				for (Location location : locations)
+				{
+					if (location instanceof StopLocation)
+					{
+						StopLocation stopLocation = (StopLocation)location;
+
+
+						for (String route : stopLocation.getRoutes())
+						{
+							if (isCommuterRail(route) && containsRoute(route, outputData) == false)
+							{
+								String url = routesToUrls.get(route);
+								outputData.add(new RefreshData(url, route));
+							}
+						}
+					}
+					else if (location instanceof BusLocation)
+					{
+						//bus location
+						BusLocation busLocation = (BusLocation)location;
+						String route = busLocation.getRouteId();
+
+						if (isCommuterRail(route) && containsRoute(route, outputData) == false)
+						{
+							String url = routesToUrls.get(route);
+							outputData.add(new RefreshData(url, route));
+						}
+					}
+				}
+			}
+			else
+			{
+				//add all 12 of them
+
+				for (String route : routesToUrls.keySet())
+				{
+					String url = routesToUrls.get(route);
+					
+					outputData.add(new RefreshData(url, route));
+				}
+			}
+		}
+	}
+
+	private static boolean containsRoute(String route, List<RefreshData> outputData) {
+		boolean containsRoute = false;
+		for (RefreshData row : outputData) {
+			if (row.route.equals(route)) {
+				containsRoute = true;
+				break;
+			}
+		}
+		return containsRoute;
+	}
+
+	private boolean isCommuterRail(String routeName) {
+		return routeTitles.hasRoute(routeName);
+	}
+
+	@Override
+	public boolean hasPaths() {
+		return false;
+	}
+
+	@Override
+	public ITransitDrawables getDrawables() {
+		return drawables;
+	}
+
+	@Override
+	public String searchForRoute(String indexingQuery, String lowercaseQuery)
+	{
+		//try splitting up the route keys along the diagonal and see if they match one piece of it
+		for (String route : routeTitles.routeTags())
+		{
+			String title = routeTitles.getTitle(route);
+			if (title.contains("/"))
+			{
+				String[] pieces = title.split("/");
+				for (int i = 0; i < pieces.length; i++)
+				{
+					if (lowercaseQuery.equals(pieces[i].toLowerCase()))
+					{
+						return route;
+					}
+				}
+			}
+		}
+		
+		return SearchHelper.naiveSearch(indexingQuery, lowercaseQuery, routeTitles);
+		
+	}
+
+	@Override
+	public CommuterRailStopLocation createStop(float latitude, float longitude,
+			String stopTag, String stopTitle,
+			String route) {
+		CommuterRailStopLocation stop = new CommuterRailStopLocation.CommuterRailBuilder(
+				latitude, longitude, stopTag, stopTitle).build();
+		stop.addRoute(route);
+		return stop;
+	}
+
+	@Override
+	public int getLoadOrder() {
+		return 3;
+	}
+
+    @Override
+    public int[] getTransitSourceIds() {
+        return new int[] {Schema.Routes.enumagencyidCommuterRail};
+    }
+
+    @Override
+	public TransitSourceTitles getRouteTitles() {
+		return routeTitles;
+	}
+
+	@Override
+	public boolean requiresSubwayTable() {
+		return true;
+	}
+
+	@Override
+	public IAlerts getAlerts() {
+		return transitSystem.getAlerts();
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Commuter Rail";
+	}
+}
