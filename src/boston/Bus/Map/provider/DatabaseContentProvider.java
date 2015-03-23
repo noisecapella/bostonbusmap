b355cc2aa16c187cce5bea3456ebada911807276
George Schneeloch <bostonbusmap@gmail.com>
2015-03-22 23:31:32 -0400

    Inc version

--- src/boston/Bus/Map/provider/DatabaseContentProvider.java
+++ src/boston/Bus/Map/provider/DatabaseContentProvider.java
@@ -139,7 +139,8 @@ public class DatabaseContentProvider extends ContentProvider {
 	public final static int HUBWAY_15 = 64;
 	public final static int HUBWAY_16 = 65;
 	public final static int HUBWAY_17 = 66;
-	public final static int CURRENT_DB_VERSION = HUBWAY_17;
+	public final static int HUBWAY_18 = 67;
+	public final static int CURRENT_DB_VERSION = HUBWAY_18;
 
 	public static final int ALWAYS_POPULATE = 3;
 	public static final int POPULATE_IF_UPGRADE = 2;
