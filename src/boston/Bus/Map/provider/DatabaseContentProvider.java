899032a19192e99472b911710dd2ac278fc36951
George Schneeloch <noisecapella@gmail.com>
2015-12-15 08:58:47 -0500

    Inc database version

--- src/boston/Bus/Map/provider/DatabaseContentProvider.java
+++ src/boston/Bus/Map/provider/DatabaseContentProvider.java
@@ -128,7 +128,8 @@ public class DatabaseContentProvider extends ContentProvider {
 	public final static int NEW_DB_9 = 48;
 	public final static int NEW_DB_10 = 49;
     public final static int NEW_DB_11 = 50;
-	public final static int CURRENT_DB_VERSION = NEW_DB_11;
+	public final static int NEW_DB_12 = 51;
+	public final static int CURRENT_DB_VERSION = NEW_DB_12;
 
 	public static final int ALWAYS_POPULATE = 3;
 	public static final int POPULATE_IF_UPGRADE = 2;
