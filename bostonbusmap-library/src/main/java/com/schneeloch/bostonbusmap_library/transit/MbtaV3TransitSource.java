8639ecc6c239964e423e7e311bb93016bdbef0c8
George Schneeloch <bostonbusmap@gmail.com>
2018-01-08 23:15:30 -0500

    Add MBTA APIv3 TransitSource

---
+++ bostonbusmap-library/src/main/java/com/schneeloch/bostonbusmap_library/transit/MbtaV3TransitSource.java
@@ -0,0 +1,238 @@
+package com.schneeloch.bostonbusmap_library.transit;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.schneeloch.bostonbusmap_library.data.BusLocation;
+import com.schneeloch.bostonbusmap_library.data.Directions;
+import com.schneeloch.bostonbusmap_library.data.IAlerts;
+import com.schneeloch.bostonbusmap_library.data.ITransitDrawables;
+import com.schneeloch.bostonbusmap_library.data.Location;
+import com.schneeloch.bostonbusmap_library.data.Locations;
+import com.schneeloch.bostonbusmap_library.data.RouteConfig;
+import com.schneeloch.bostonbusmap_library.data.RoutePool;
+import com.schneeloch.bostonbusmap_library.data.RouteStopPair;
+import com.schneeloch.bostonbusmap_library.data.Selection;
+import com.schneeloch.bostonbusmap_library.data.StopLocation;
+import com.schneeloch.bostonbusmap_library.data.TransitSourceCache;
+import com.schneeloch.bostonbusmap_library.data.TransitSourceTitles;
+import com.schneeloch.bostonbusmap_library.data.VehicleLocations;
+import com.schneeloch.bostonbusmap_library.database.Schema;
+import com.schneeloch.bostonbusmap_library.parser.MbtaV3PredictionsParser;
+import com.schneeloch.bostonbusmap_library.parser.MbtaV3VehiclesParser;
+import com.schneeloch.bostonbusmap_library.util.IDownloadHelper;
+import com.schneeloch.bostonbusmap_library.util.IDownloader;
+import com.schneeloch.bostonbusmap_library.util.SearchHelper;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by schneg on 12/14/17.
+ */
+
+public class MbtaV3TransitSource implements TransitSource {
+    private final TransitSourceCache cache;
+    private final ITransitDrawables drawables;
+    private final TransitSourceTitles routeTitles;
+    private final ITransitSystem transitSystem;
+    private final IDownloader downloader;
+    public static final String apiKey = "109fafba79a848e792e8e7c584f6d1f1";
+    public static final String dataUrlPrefix = "https://api-v3.mbta.com";
+
+    public MbtaV3TransitSource(ITransitDrawables drawables,
+                               TransitSourceTitles routeTitles,
+                               ITransitSystem transitSystem,
+                               IDownloader downloader) {
+        this.drawables = drawables;
+        this.routeTitles = routeTitles;
+        this.transitSystem = transitSystem;
+        this.downloader = downloader;
+        cache = new TransitSourceCache();
+    }
+
+
+    @Override
+    public void refreshData(RouteConfig routeConfig, Selection selection, int maxStops, double centerLatitude, double centerLongitude, VehicleLocations busMapping, RoutePool routePool, Directions directions, Locations locationsObj) throws IOException, ParserConfigurationException, SAXException {
+        //read data from the URL
+        ImmutableList<ImmutableList<Location>> groups = locationsObj.getLocations(maxStops, centerLatitude, centerLongitude, false, selection);
+
+        ITransitSystem transitSystem = locationsObj.getTransitSystem();
+        IDownloadHelper downloadHelper;
+        Selection.Mode mode = selection.getMode();
+        switch (mode) {
+            case BUS_PREDICTIONS_ONE:
+            case BUS_PREDICTIONS_STAR:
+            case BUS_PREDICTIONS_ALL: {
+
+                List<String> stopIds = Lists.newArrayList();
+                for (ImmutableList<Location> group : groups) {
+                    for (Location location : group) {
+                        if (!(location instanceof StopLocation)) {
+                            // might happen if there's a race condition somewhere
+                            continue;
+                        }
+                        StopLocation stopLocation = (StopLocation) location;
+                        for (String route : location.getRoutes()) {
+                            Schema.Routes.SourceId sourceId = transitSystem.getSourceId(route);
+                            if (sourceId == Schema.Routes.SourceId.Subway || sourceId == Schema.Routes.SourceId.CommuterRail) {
+                                stopIds.add(stopLocation.getStopTag());
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                //ok, do predictions now
+
+                String url = dataUrlPrefix + "/predictions?api_key=" + apiKey + "&filter[stop]=" + Joiner.on(",").join(stopIds) + "&include=vehicle,trip";
+                downloadHelper = downloader.create(url);
+                break;
+            }
+            case VEHICLE_LOCATIONS_ONE: {
+                String url = dataUrlPrefix + "/vehicles?api_key=" + apiKey + "&filter[route]=" + routeConfig.getRouteName();
+                downloadHelper = downloader.create(url);
+                break;
+            }
+            case VEHICLE_LOCATIONS_ALL: {
+                String url = dataUrlPrefix + "/vehicles?api_key=" + apiKey;
+                downloadHelper = downloader.create(url);
+                break;
+            }
+            default:
+                throw new RuntimeException("Unexpected enum");
+        }
+
+        try {
+            InputStream data = downloadHelper.getResponseData();
+
+            switch (mode) {
+                case BUS_PREDICTIONS_ONE:
+                case BUS_PREDICTIONS_ALL:
+                case BUS_PREDICTIONS_STAR: {
+                    //bus prediction
+
+                    try {
+                        MbtaV3PredictionsParser.runParse(routeTitles, cache, routePool, groups, data);
+                    }
+                    catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    break;
+                }
+                case VEHICLE_LOCATIONS_ALL:
+                case VEHICLE_LOCATIONS_ONE: {
+                    //vehicle locations
+                    Map<VehicleLocations.Key, BusLocation> newBuses = MbtaV3VehiclesParser.runParse(data);
+
+                    busMapping.update(Schema.Routes.SourceId.Bus, routeTitles.routeTags(), true, newBuses);
+
+                    // now that we've succeeded, update last download times
+                    switch (mode) {
+                        case VEHICLE_LOCATIONS_ONE:
+                            cache.updateVehiclesForRoute(routeConfig.getRouteName());
+                            break;
+                        case VEHICLE_LOCATIONS_ALL:
+                            cache.updateAllVehicles();
+                            break;
+                        default:
+                            throw new RuntimeException("Unexpected mode");
+                    }
+
+                    break;
+                }
+                default:
+                    throw new RuntimeException("Unexpected enum");
+            }
+        }
+        finally {
+            downloadHelper.disconnect();
+        }
+    }
+
+    private ImmutableList<RouteStopPair> getStopPairs(List<Location> locations) {
+        ImmutableList.Builder<RouteStopPair> builder = ImmutableList.builder();
+
+        for (Location location : locations)
+        {
+            if (location instanceof StopLocation) {
+                StopLocation stopLocation = (StopLocation) location;
+                boolean matchesBus = false;
+                for (String route : stopLocation.getRoutes()) {
+                    if (transitSystem.getSourceId(route) == Schema.Routes.SourceId.Bus) {
+                        matchesBus = true;
+                        break;
+                    }
+                }
+                if (matchesBus) {
+
+                    for (String stopRoute : stopLocation.getRoutes()) {
+                        RouteStopPair pair = new RouteStopPair(stopRoute, stopLocation.getStopTag());
+                        if (cache.canUpdatePredictionForStop(pair)) {
+                            builder.add(pair);
+                        }
+                    }
+                }
+            }
+        }
+
+        return builder.build();
+
+    }
+
+    @Override
+    public boolean hasPaths() {
+        return true;
+    }
+
+    @Override
+    public String searchForRoute(String indexingQuery, String lowercaseQuery) {
+        return SearchHelper.naiveSearch(indexingQuery, lowercaseQuery, transitSystem.getRouteKeysToTitles());
+    }
+
+    @Override
+    public ITransitDrawables getDrawables() {
+        return drawables;
+    }
+
+    @Override
+    public StopLocation createStop(float latitude, float longitude, String stopTag, String stopTitle, String route, Optional<String> parent) {
+        StopLocation stop = new StopLocation.Builder(latitude, longitude, stopTag, stopTitle, parent).build();
+        stop.addRoute(route);
+        return stop;
+    }
+
+    @Override
+    public BusLocation createVehicleLocation(float latitude, float longitude, String id, long lastFeedUpdateInMillis, Optional<Integer> heading, String routeName, String headsign) {
+        return new BusLocation(latitude, longitude, id, lastFeedUpdateInMillis, heading, routeName, headsign);
+    }
+
+    @Override
+    public TransitSourceTitles getRouteTitles() {
+        return routeTitles;
+    }
+
+    @Override
+    public int getLoadOrder() {
+        return 3;
+    }
+
+    @Override
+    public boolean requiresSubwayTable() {
+        return true;
+    }
+
+    @Override
+    public IAlerts getAlerts() {
+        return transitSystem.getAlerts();
+    }
+}
