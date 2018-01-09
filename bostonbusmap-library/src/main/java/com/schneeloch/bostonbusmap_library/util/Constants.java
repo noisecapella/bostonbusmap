c80ac4ee9d8986686dee4dc008ca518c7e4824cd
George Schneeloch <bostonbusmap@gmail.com>
2018-01-08 22:31:21 -0500

    Add constant for ISO8601 formatting

--- bostonbusmap-library/src/main/java/com/schneeloch/bostonbusmap_library/util/Constants.java
+++ bostonbusmap-library/src/main/java/com/schneeloch/bostonbusmap_library/util/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
 	public static final int E6 = 1000000; 
 	public static final double InvE6 = 1/(double)E6;
-	
+	public static final String ISO8601 = "yyyy-MM-dd'T'HH:mm:ssX";
 }
