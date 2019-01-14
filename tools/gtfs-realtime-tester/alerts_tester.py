009721a3b9ce1e20b68fcb2e4f9b2dbf5a820419
George Schneeloch <bostonbusmap@gmail.com>
2019-01-14 08:36:48 -0500

    Use requests

--- tools/gtfs-realtime-tester/alerts_tester.py
+++ tools/gtfs-realtime-tester/alerts_tester.py
@@ -1,21 +1,13 @@
+import requests
 import sys
 import gtfs_realtime_pb2
 
-from twisted.internet import reactor
-from twisted.internet.defer import inlineCallbacks
-from twisted.web.client import getPage
-
-@inlineCallbacks
 def main():
-
-    data = yield getPage("http://developer.mbta.com/lib/gtrtfs/Alerts/Alerts.pb")
+    data = requests.get("https://cdn.mbta.com/realtime/Alerts.pb").content
     update = gtfs_realtime_pb2.FeedMessage()
     update.ParseFromString(data)
 
-    print update
-
-    reactor.stop()
+    print(update)
 
 if __name__ == "__main__":
-    reactor.callLater(0, main)
-    reactor.run()
+    main()
