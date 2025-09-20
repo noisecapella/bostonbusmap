71071c9d34ad78fcc87df3141203ee2bd8950ea4
George Schneeloch <noisecapella@gmail.com>
2025-09-20 11:45:35 -0400

    delete some files

--- src/main/java/com/schneeloch/bostonbusmap_library/parser/gson/gbfs/info/Data.java
+++
@@ -1,8 +0,0 @@
-package com.schneeloch.bostonbusmap_library.parser.gson.gbfs.info;
-
-import com.google.gson.annotations.SerializedName;
-
-public class Data {
-    @SerializedName("en")
-    public Feeds en;
-}
--- src/main/java/com/schneeloch/bostonbusmap_library/parser/gson/gbfs/info/Root.java
+++
@@ -1,8 +0,0 @@
-package com.schneeloch.bostonbusmap_library.parser.gson.gbfs.info;
-
-import com.google.gson.annotations.SerializedName;
-
-public class Root {
-    @SerializedName("data")
-    public Data data;
-}
